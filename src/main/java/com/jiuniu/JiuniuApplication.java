d5861499981ef306ad31e65b419687609f2d67e3
John <496461090@qq.com>
2019-11-13 18:54:51 +0800

    Initial commit

---
+++ src/main/java/com/jiuniu/JiuniuApplication.java
@@ -0,0 +1,13 @@
+package com.jiuniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiuniuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiuniuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiuniu/JiuniuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiuniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiuniuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
