8c2be988b00ba5b55241904206b0411cb0e5d455
zhangzhan <496461090@qq.com>
2019-11-20 15:54:00 +0800

    修改端口

--- src/main/java/com/jiuniu/controller/HelloController.java
+++ src/main/java/com/jiuniu/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping("*")
     public String hello(HttpServletRequest request, @RequestParam(value = "name", defaultValue = "springboot-thymeleaf") String name) {
         request.setAttribute("name", name);
         return "hello";
